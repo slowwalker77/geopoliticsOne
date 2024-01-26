84985362198f363a1d3d77028120658460257394
slowwalker77 <90623845+slowwalker77@users.noreply.github.com>
2024-01-26 19:04:49 +0900

    first commit

--- readinggeopolitics/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['cdn.steemitimages.com'],
-  },
-  reactStrictMode: true,
-};
-
-module.exports = nextConfig;
--- readinggeopolitics/src/app/page.js
+++
@@ -1,9 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function Home() {
-  <span className='sr-obly'>
-    Home 페이지에 내용 넣기 전까지 국내문제로 redirect
-  </span>;
-  redirect('/blogs/section-1');
-  return;
-}
